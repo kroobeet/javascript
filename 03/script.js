2130121c8b49feae1743f5b6f03c570235f09e5a
kroobeet <kroobeet@gmail.com>
2020-08-17 13:40:13 +0600

    add JavaScript 03

---
+++ 03/script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").addEventListener("click", function () {
+    document.getElementById("demo").innerHTML = "Hello Everyone!";
+});
+
+function myFunc () {
+    document.getElementById("demo2").innerHTML = "Hello Everyone!";
+}
