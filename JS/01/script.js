16e42c37402cf71665dc5653ec46301a149af853
kroobeet <kroobeet@gmail.com>
2020-08-26 17:16:59 +0600

    update structure

--- JS/01/script.js
+++
@@ -1,8 +0,0 @@
-function changeColor(element) {
-    let currentColor = element.style.backgroundColor;
-    if (currentColor == "red") {
-        element.style.backgroundColor = "green";
-    } else {
-        element.style.backgroundColor = "red";
-    }
-}
--- JS/03/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("btn").addEventListener("click", function () {
-    document.getElementById("demo").innerHTML = "Hello Everyone!";
-});
-
-function myFunc () {
-    document.getElementById("demo2").innerHTML = "Hello Everyone!";
-}
