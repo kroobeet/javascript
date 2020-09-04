53f93ac5cef1ca0a1d5c8e146bd1e7d136691714
kroobeet <kroobeet@gmail.com>
2020-09-04 17:02:09 +0600

    save

--- JS/0_3/1/script.js
+++ JS/0_3/1/script.js
@@ -1,17 +1,52 @@
-function buttonClick() {
-	var input = document.getElementById('input1');
-	input.value = '!!!';
+const input = document.getElementById('inp');
+var str = input.value;
+var resultNum = 0;
+var arr = [];
+
+function elemCalc(elem) {
+	input.value += elem.value;
+	arr.push(elem.value);
+	if (!Number(arr[0])) {
+		alert('First only NUMBER');
+		clearValue();
+	}
 }
 
-function showInputValue() {
-	var input = document.getElementById('input2');
-	var value = input.value;
-	alert(value);
+function del() {
+	arr.pop();
 }
 
-function squareNum() {
-	var input = document.getElementById('input3');
-	var value = Number(input.value);
-	alert(value*value);
+function clearValue() {
+	arr = [];
+	input.value = '';
 }
 
+function check() {
+	alert(arr);
+}
+
+function prevNum() {
+	for (let i = 0; i < str.length; i++) {
+		
+	}
+}
+
+function nextNum() {
+
+}
+
+function checkSymbol() {
+	for (let i = 0; i < str.length; i++) {
+		if (Number(str[i]) == false) {
+			var symbol = str[i];
+			return symbol;
+		} else {
+			i++;
+		}
+	}
+	return false;
+}
+
+function result() {
+	
+}
